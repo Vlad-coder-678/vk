05d4242ebc541a7bf04729db73eb6e88653fe1d9
Vlad-coder-678 <64953681+Vlad-coder-678@users.noreply.github.com>
2020-12-08 17:19:51 +0300

    Add files via upload

---
+++ src/Components/Header/HeaderContainer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import { connect } from "react-redux";
+import { getAuthUserDataTC } from "../../redux/auth-reducer";
+
+class HeaderContainer extends React.Component {
+  componentDidMount() {
+    this.props.getAuthUserDataTC();
+  }
+  render() {
+    return <Header {...this.props} />;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+  login: state.auth.login,
+});
+
+export default connect(mapStateToProps, { getAuthUserDataTC })(HeaderContainer);
